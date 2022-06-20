e954e3d7cb684365b84253a3d46bb94b423d247e
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-06-20 11:09:07 -0400

    finished js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -25,7 +25,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === "none") {
+  if (license === "None") {
     return "";
   } else if (!license) {
     return "";
@@ -38,7 +38,7 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
   let license = data.license;
   return `# ${data.title}
-
+  
   <br />
 
   ##Table of Contents
